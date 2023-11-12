d847beeff5dbdaeec80035d156e4ed057cd1efa1
schunduru256 <146134857+schunduru256@users.noreply.github.com>
2023-11-11 19:45:32 -0500

    Add files via upload

---
+++ labSolution.py
@@ -0,0 +1,10 @@
+############################################################
+# Name: Samhita Chundutu
+# Pledge: I pledge my honor that I have abided by the Stevens Honor System.
+# CS115 Lab 1
+#  
+############################################################
+def same(word):
+    return(word[0].lower() == word[-1].lower())
+def consecutiveSum(x, y):
+    return((x+y)/2*(y-x-1))
